89ba93821a3af87f12ef77501990584ce2fddd4a
atrem <Bobcopy555@gmail.com>
2022-07-21 18:18:32 +0500

    CrudRepository to JpaRepository

--- src/main/java/com/example/filevault/repository/DateBaseFileRepository.java
+++ src/main/java/com/example/filevault/repository/DateBaseFileRepository.java
@@ -1,11 +1,12 @@
 package com.example.filevault.repository;
 
 import com.example.filevault.entity.FileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface DateBaseFileRepository extends CrudRepository<FileEntity, UUID> {
+public interface DateBaseFileRepository extends JpaRepository<FileEntity, UUID> {
     List<FileEntity> findAll();
 }
