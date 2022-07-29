c4ae81254a74a6f36d401a2dc040da9f4d950556
atrem <Bobcopy555@gmail.com>
2022-07-29 15:42:40 +0500

    fake database implementation is deleted

--- src/main/java/com/example/filevault/repository/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.filevault.repository;
-
-import org.springframework.security.core.userdetails.User;
-
-import java.util.Optional;
-
-public interface UserDao {
-    Optional<User> selectApplicationUserByUsername(String username);
-}
