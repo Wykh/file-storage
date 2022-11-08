ce6717cad92bbf166be97677c2fed7f025a570a7
atrem <bocopy555@gmail.com>
2022-11-08 20:24:41 +0500

    now test return string with all files

--- src/test/java/com/example/filevault/FileVaultApplicationTests.java
+++ src/test/java/com/example/filevault/FileVaultApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.filevault;
 
+import com.example.filevault.controller.FileController;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+//@WebMvcTest(FileController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class FileVaultApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private FileController fileController;
+
 	@Test
-	void contextLoads() {
+	void test() throws Exception {
+		// create post request to get jwt token using json body
+		ResultActions resultActions = mockMvc.perform(post("/login")
+				.contentType("application/json")
+				.content("{\"username\":\"admin\",\"password\":\"admin\"}"))
+				.andExpect(status().isOk());
+
+		// get jwt token from response from authentication header
+		String token = resultActions.andReturn().getResponse().getHeader("Authorization");
+
+		System.out.println(token);
+
+		// create get request to get all files
+		ResultActions authorization = mockMvc.perform(get("/api/file")
+						.header("Authorization", token))
+				.andExpect(status().isOk());
+
+		// get all files from response get content
+		String content = authorization.andReturn().getResponse().getContentAsString();
+
 	}
 
 }
